f27889a85fba17e23079fd238b56ccb9aafb486b
Jeet <itsjeetusaini@gmail.com>
2023-03-02 12:19:11 +0530

    uploading

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,21 +1,60 @@
-function playSound(e) {
+
+const imagesUrls = {
+  65:"https://arsen-nazaryan.github.io/drum_kit/media/icons/clap.png",
+83:"https://arsen-nazaryan.github.io/drum_kit/media/icons/snare.png",
+68:"https://arsen-nazaryan.github.io/drum_kit/media/icons/kick.png",
+70:"https://arsen-nazaryan.github.io/drum_kit/media/icons/hihat.png",
+74:"https://arsen-nazaryan.github.io/drum_kit/media/icons/boom.png",
+75:"https://arsen-nazaryan.github.io/drum_kit/media/icons/tom.png",
+76:"https://arsen-nazaryan.github.io/drum_kit/media/icons/tink.png",
+18:"https://arsen-nazaryan.github.io/drum_kit/media/icons/openhat.png",
+  };
+
+const restart = document.querySelector(".button");
+const music_box = document.querySelector(".music_box");
+const container = document.querySelector(".container");
+
+restart.addEventListener("click", nextScreen);
+function nextScreen(){
+  music_box.style.display = "flex";
+  container.style.display = "none";
+  
+}
+
+function playSounds(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  const image = document.querySelector(".image");
 
-  if (!audio) return; // stop the function from running all together
-  audio.currentTime = 0; // rewing to the start
+  if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
-  key.classList.add('playing');
+  key.classList.add("playing");
+  image.style.background = `url("${imagesUrls[e.keyCode]}")`;
+}
+
+function soundsTap(e) {
+  var el = this.id;
+   const image = document.querySelector(".image");
+
+  const audio = document.querySelector(`audio[data-key="${el}"]`);
+  if (!audio) return;
+  image.style.background = `url("${imagesUrls[el]}")`;
+  audio.currentTime = 0;
+  audio.play();
 }
 
 function removeTransition(e) {
-  if (e.propertyName !== 'transform') return; // skip it if it's not a transform
-  this.classList.remove('playing');
-  console.log(this);
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll('.key');
+const key_btn = document.querySelectorAll(".key");
 
-// event listeners
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+key_btn.forEach((key) =>
+  key.addEventListener("transitionend", removeTransition)
+);
+
+key_btn.forEach((el) => el.addEventListener("click", soundsTap));
+
+window.addEventListener("keydown", playSounds);
--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
-const walk = 100; // 100px
+const walk =600
 
 function shadow(e) {
   // const width = hero.offsetWidth;
@@ -27,12 +27,12 @@ function shadow(e) {
 
   text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(0,255,0,0.7)`;
 
-  // text.style.textShadow = `
-  //   ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
-  //   ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
-  //   ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
-  //   ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
-  // `;
+  text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+    ${yWalk}px ${xWalk * 1}px 0 rgba(0,255,0,0.7),
+    ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+  `;
 }
 
 hero.addEventListener('mousemove', shadow);
